fa3ee14f76fed0f619bd7d5a6246eb34ff6d8974
gustavocadev <churaalaygustavo11@gmail.com>
2023-03-30 11:45:04 -0500

    feat: add authjs

--- src/routes/plugin@auth.ts
+++ src/routes/plugin@auth.ts
@@ -2,6 +2,7 @@ import { serverAuth$ } from '@builder.io/qwik-auth';
 import Credentials from '@auth/core/providers/credentials';
 import type { Provider } from '@auth/core/providers';
 import { prisma } from '~/server/prisma';
+import bcryptjs from 'bcryptjs';
 
 export const { onRequest, useAuthSession, useAuthSignin, useAuthSignout } =
   serverAuth$(({ env }) => ({
@@ -10,17 +11,29 @@ export const { onRequest, useAuthSession, useAuthSignin, useAuthSignout } =
     providers: [
       Credentials({
         name: 'credentials',
-        authorize: async (credentials) => {
-          prisma.customer.findFirst({
-            where: {},
+        authorize: async (credentials: any) => {
+          const customer = await prisma.customer.findFirst({
+            where: {
+              email: credentials.email,
+            },
           });
-          console.log(credentials);
-          console.log('authorize');
-          return null;
+          if (!customer) {
+            return null;
+          }
+
+          const isPasswordValid = await bcryptjs.compare(
+            credentials.password,
+            customer.password
+          );
+
+          if (!isPasswordValid) {
+            return null;
+          }
+          return customer;
         },
         credentials: {
           username: {
-            label: 'Username',
+            label: 'Email',
             type: 'text',
           },
           password: {
