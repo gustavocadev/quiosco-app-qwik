d07a7b36e2efe363db4f81523bedcff8e27ab874
gustavocadev <churaalaygustavo11@gmail.com>
2023-04-30 10:28:12 -0500

    chore: add a lucia-auth based login

--- src/routes/(unauthed)/signup/index.tsx
+++ src/routes/(unauthed)/signup/index.tsx
@@ -1,10 +1,47 @@
 import { component$ } from '@builder.io/qwik';
-import { Form, routeAction$, z, zod$ } from '@builder.io/qwik-city';
+import {
+  Form,
+  routeAction$,
+  routeLoader$,
+  z,
+  zod$,
+} from '@builder.io/qwik-city';
+import { auth } from '~/lib/lucia';
 
-export const useActionCreateCustomer = routeAction$(
-  (values) => {
-    console.log(values);
-    return {};
+export const useSignupLoader = routeLoader$(async (event) => {
+  const authRequest = auth.handleRequest(event);
+  const { user } = await authRequest.validateUser();
+
+  // if user is already logged in, redirect to /category/cafe
+  if (user) {
+    throw event.redirect(303, '/category/cafe');
+  }
+
+  return {};
+});
+
+export const useSignupAction = routeAction$(
+  async (values, event) => {
+    const username = `${values.firstName}${Date.now()}`.toLowerCase();
+    await auth.createUser({
+      primaryKey: {
+        // my credentials
+        providerId: 'email',
+        providerUserId: values.email,
+        password: values.password,
+      },
+      // custom attributes
+      attributes: {
+        email: values.email,
+        name: `${values.firstName}`,
+        lastName: `${values.lastName}`,
+        username: username,
+        phone: '',
+        address: '',
+      },
+    });
+
+    throw event.redirect(303, '/');
   },
   zod$({
     firstName: z.string().min(2),
@@ -15,7 +52,7 @@ export const useActionCreateCustomer = routeAction$(
 );
 
 export default component$(() => {
-  const actionCreateCustomer = useActionCreateCustomer();
+  const signupAction = useSignupAction();
   return (
     <div class="max-w-2xl mx-auto border mt-20 p-4">
       <section class="flex gap-8 items-center flex-wrap">
@@ -28,38 +65,42 @@ export default component$(() => {
             <h1 class="font-bold text-3xl">Sign up</h1>
           </header>
 
-          <Form class="flex flex-col gap-2 p-2" action={actionCreateCustomer}>
-            <label>
+          <Form action={signupAction} class="flex flex-col gap-2 p-2">
+            <label for="firstName">
               <span>First name</span>
             </label>
             <input
+              id="firstName"
               type="text"
               class="p-2 rounded border bg-gray-50 w-full"
               name="firstName"
             />
 
-            <label for="">
+            <label for="lastName">
               <span>Last name</span>
             </label>
             <input
+              id="lastName"
               type="text"
               class="p-2 rounded border bg-gray-50"
               name="lastName"
             />
 
-            <label for="">
+            <label for="email">
               <span>Email</span>
             </label>
             <input
+              id="email"
               type="email"
               class="p-2 rounded border bg-gray-50"
               name="email"
             />
 
-            <label for="">
+            <label for="password">
               <span>Password</span>
             </label>
             <input
+              id="password"
               type="password"
               class="p-2 rounded border bg-gray-50"
               name="password"
