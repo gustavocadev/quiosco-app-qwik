c2ee505e7aff79fe55ad49d4af14c848c8c6ac7c
gustavocadev <churaalaygustavo11@gmail.com>
2023-04-30 10:29:33 -0500

    chore: load categories

--- src/routes/(authed)/layout.tsx
+++ src/routes/(authed)/layout.tsx
@@ -1,20 +1,44 @@
 import { component$, Slot } from '@builder.io/qwik';
 import { Sidebar } from '~/components/ui/Sidebar';
 import { routeLoader$ } from '@builder.io/qwik-city';
-import { prisma } from '~/server/prisma';
+import { prisma } from '~/lib/prisma';
 import { Steps } from '~/components/ui/Steps';
+import { auth } from '~/lib/lucia';
 
-export const useLoaderCategories = routeLoader$(() => {
+export const useLoaderCategories = routeLoader$(async () => {
   return prisma.category.findMany();
+});
+
+export const useLoaderUser = routeLoader$(async (event) => {
+  const authRequest = auth.handleRequest(event);
+  const { user } = await authRequest.validateUser();
+  // if user is already logged in, redirect to login page
+  if (!user) {
+    throw event.redirect(303, '/');
+  }
+
+  const userDB = await prisma.authUser.findUnique({
+    where: {
+      id: user.userId,
+    },
+  });
+
+  if (!userDB) {
+    throw event.redirect(303, '/');
+  }
+
+  return {
+    username: userDB.username,
+  };
 });
 
 export default component$(() => {
   const categories = useLoaderCategories();
-
+  const user = useLoaderUser();
   return (
     <section class="md:flex">
       <aside class="h-screen overflow-y-scroll md:w-4/12 xl:w-1/4 2xl:w-1/5">
-        <Sidebar categories={categories.value} />
+        <Sidebar categories={categories.value} username={user.value.username} />
       </aside>
       <main class="h-screen overflow-y-scroll md:w-8/12 xl:w-3/4 2xl:w-4/5">
         <section class="p-10">
